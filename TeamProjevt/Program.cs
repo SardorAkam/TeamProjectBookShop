3adb44a74582fbf3c1e65386ccef1ea55ae3039a
User <User@DESKTOP-5EVRPB5>
2025-02-26 12:56:02 +0500

    Добавьте файлы проекта.

---
+++ TeamProjevt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Channels;
+
+namespace Bookshop
+{
+    class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                
+            }
+        }
+    }
+}
+
